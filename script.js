c29052fcc8ef27ae5ec7777e7254d015354bb17c
Thiago <thiago-470@outlook.com>
2022-09-14 19:50:37 -0300

    diminuindo lint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const btnAlerta = document.getElementById("btnAlerta");
-const emailHeader = document.getElementById("email");
-const senhaHeader = document.getElementById("senha");
+const btnAlerta = document.getElementById('btnAlerta');
+const emailHeader = document.getElementById('email');
+const senhaHeader = document.getElementById('senha');
 
 btnAlerta.addEventListener('click', () => {
 if (emailHeader.value === 'tryber@teste.com' && senhaHeader.value === '123456') {
