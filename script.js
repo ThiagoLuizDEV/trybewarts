509b79bf21528b3aa19ea6127a5fcb402f081f6e
Lucas Silva <lucasfic2017@gmail.com>
2022-09-14 17:55:47 -0300

    adiciona o requisito 8

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-const btnAlerta = document.getElementById("btn-alerta");
+
+const btnAlerta = document.getElementById("btnAlerta");
 const emailHeader = document.getElementById("email");
 const senhaHeader = document.getElementById("senha");
 
 btnAlerta.addEventListener("click", () => {
-=======
-const btnAlerta = document.getElementById('btn-alerta');
-const emailHeader = document.getElementById('email');
-const senhaHeader = document.getElementById('senha');
-btnAlerta.addEventListener('click', () => {
->>>>>>> e605b4af758df681b6f8e1ea33935bdd418223d9
   if (emailHeader.value === 'tryber@teste.com' && senhaHeader.value === '123456') {
     alert('Olá, Tryber!');
   } else {
