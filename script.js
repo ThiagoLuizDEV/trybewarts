0d983397ae889b6bb264fda54be1e2d32b5edbe8
Lucas Silva <lucasfic2017@gmail.com>
2022-09-18 20:05:17 -0300

    adiciona os requisitos 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnAlerta = document.getElementById('btnAlerta');
 const emailHeader = document.getElementById('email');
 const senhaHeader = document.getElementById('senha');
+const agreement = document.getElementById('agreement');
+const btnEnviar = document.getElementById('submit-btn');
 
 btnAlerta.addEventListener('click', () => {
   if (emailHeader.value === 'tryber@teste.com' && senhaHeader.value === '123456') {
@@ -8,4 +10,8 @@ btnAlerta.addEventListener('click', () => {
   } else {
     alert('Email ou senha inválidos.');
   }
+});
+
+agreement.addEventListener('change', () => {
+  btnEnviar.disabled = !agreement.checked;
 });
