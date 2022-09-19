cc75fe7ecebc632b5c29693c783ccf05fae6eb8d
Lucas Silva <lucasfic2017@gmail.com>
2022-09-19 00:46:05 -0300

    adiciona o requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const emailHeader = document.getElementById('email');
 const senhaHeader = document.getElementById('senha');
 const agreement = document.getElementById('agreement');
 const btnEnviar = document.getElementById('submit-btn');
+const contador = document.getElementById('counter');
+const textarea = document.getElementById('textarea');
+const limite = contador.innerText;
 
 btnAlerta.addEventListener('click', () => {
   if (emailHeader.value === 'tryber@teste.com' && senhaHeader.value === '123456') {
@@ -14,4 +17,10 @@ btnAlerta.addEventListener('click', () => {
 
 agreement.addEventListener('change', () => {
   btnEnviar.disabled = !agreement.checked;
+});
+
+textarea.addEventListener('input', () => {
+  const caracteresDigitados = textarea.value.length;
+  const caracteresRestantes = limite - caracteresDigitados;
+  contador.innerText = caracteresRestantes;
 });
